e827b99dccf4cc2cdfb608897f12b421b8a186b0
AndriiAvramenk <72814303+AndriiAvramenk@users.noreply.github.com>
2026-03-18 22:04:01 +0200

    Fc

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node }},
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  globalIgnores(["node_modules/","coverage/","mochawesome-report/"]),
+  {rules: {
+        "class-methods-use-this": "off",
+        "arrow-parens": "off",
+        "prefer-arrow-callback": "off",
+        "func-names": "off",
+        "object-curly-newline": "off",
+        "no-undef": "warn"
+  }}
+]);
